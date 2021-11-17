a1083fbd56c46098a1d9dcc3e94e0716b9bc75bf
francisco.pardo <pardofrancisco2001@gmail.com>
2021-11-17 18:46:03 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Sitio en mantenimiento!
         </p>
         <a
           className="App-link"

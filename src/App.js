a295fdb75751caf357a71f465c776c594daea069
francisco.pardo <pardofrancisco2001@gmail.com>
2021-11-22 18:53:13 -0300

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           ¡Sitio en mantenimiento!
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+const NavBar = () => {
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/#">Mi Tienda</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/#">Vehículos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="/#">Accesorios</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link active dropdown-toggle" href="/#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">Marcas</a>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a className="dropdown-item" href="/#">Action</a></li>
+                                <li><a className="dropdown-item" href="/#">Another action</a></li>
+                                <li><a className="dropdown-item" href="/#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+
+}
+
+export default NavBar;
